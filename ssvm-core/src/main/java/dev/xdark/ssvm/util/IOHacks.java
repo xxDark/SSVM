6f895537ea0546194c114a0e78684e7a35818f52
Justus Garbe <gihihoh@gmail.com>
2022-11-02 22:10:06 +0100

    Make io hack independent of SharedSecrets

--- ssvm-core/src/main/java/dev/xdark/ssvm/util/IOHacks.java
+++ ssvm-core/src/main/java/dev/xdark/ssvm/util/IOHacks.java
@@ -1,22 +1,42 @@
 package dev.xdark.ssvm.util;
 
 import lombok.experimental.UtilityClass;
-import sun.misc.JavaIOFileDescriptorAccess;
-import sun.misc.SharedSecrets;
 
 import java.io.FileDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 @UtilityClass
 public class IOHacks {
 
-    private final JavaIOFileDescriptorAccess fdAccess = SharedSecrets.getJavaIOFileDescriptorAccess();
-
     public long getHandleOrFd(FileDescriptor fd) {
         try {
-            return fdAccess.getHandle(fd);
-        } catch (UnsupportedOperationException e) {
-            return fdAccess.get(fd);
+            return fdHandle == null ? fdField.getInt(fd) : fdHandle.getLong(fd);
+        } catch (IllegalAccessException e) {
+            throw new IllegalStateException("Unable to get handle from FileDescriptor", e);
         }
+    }
+
+    private final Field fdField;
+    private final Field fdHandle;
+
+    static {
+        Field fd = null;
+        Field handle = null;
+        try {
+            fd = FileDescriptor.class.getDeclaredField("fd");
+            fd.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            // ignore
+        }
+        try {
+            handle = FileDescriptor.class.getDeclaredField("handle");
+            handle.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            // ignore
+        }
+        fdField = fd;
+        fdHandle = handle;
     }
 
 }

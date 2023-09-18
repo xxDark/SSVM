86d3d43ddf6e62ff8fd8a3086fd8c0f65910ae85
Col-E <mcoley2@gmu.edu>
2023-09-18 05:52:49 -0400

    Make argument subtypes public, add type exposure

--- ssvm-invoke/src/main/java/dev/xdark/ssvm/invoke/ReferenceArgument.java
+++ ssvm-invoke/src/main/java/dev/xdark/ssvm/invoke/ReferenceArgument.java
@@ -2,22 +2,32 @@ package dev.xdark.ssvm.invoke;
 
 import dev.xdark.ssvm.execution.Locals;
 import dev.xdark.ssvm.value.ObjectValue;
+import org.objectweb.asm.Type;
 
 /**
  * Reference argument.
  *
  * @author xDark
  */
-final class ReferenceArgument implements Argument {
+public final class ReferenceArgument implements Argument {
 	private final ObjectValue value;
 
 	ReferenceArgument(ObjectValue value) {
 		this.value = value;
 	}
 
+	public ObjectValue getValue() {
+		return value;
+	}
+
 	@Override
 	public int store(Locals locals, int index) {
 		locals.setReference(index, value);
 		return 1;
+	}
+
+	@Override
+	public Type getType() {
+		return value.getJavaClass().getType();
 	}
 }
